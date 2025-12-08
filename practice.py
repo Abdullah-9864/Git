fe7f805626c07041eea3e5c8f28a3dd5228aa957
Abdullah-9864 <abdullahshahid9864@gmail.com>
2025-12-08 10:45:54 +0500

    .
    Signed-off-by: Abdullah-9864 <abdullahshahid9864@gmail.com>

---
+++ practice.py
@@ -0,0 +1,15 @@
+variable="awesome"
+def print_function():
+    global variable
+    variable="hello world"
+    print(variable)
+print_function()
+print(variable)
+y=12E3
+print(type(y))
+import random
+print(random.randrange(1,10))
+a="hello"
+print(a)
+    
+    

6e5373dffcbb35314de77f96a717aced47366cbd
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-02-18 17:39:25 +0500

    files added

---
+++ Drills/JS/freeCodeCamp/three.js
@@ -0,0 +1,2 @@
+array=[[1, 2, 3], [4, 5, 6], [[7, 8, 9],9,8]];
+console.log(array[2][0][1]); // this will give us 8 because array[2] will give us [[7, 8, 9],9,8] and then array[2][0] will give us [7, 8, 9] and then array[2][0][1] will give us 8
--- Drills/JS/freeCodeCamp/two.js
+++ Drills/JS/freeCodeCamp/two.js
@@ -12,4 +12,19 @@ c=c+" string"; // this is called concatenation
 console.log(a);
 console.log(b);
 console.log(c);
-    
+
+
+let myVar=45;
+myVar++;
+console.log(myVar);
+
+
+let myVar2="i am a \"double quote\" inside a double quote string"
+
+console.log(myVar2);
+
+
+firstLetterOfString="";
+firstName="asad";
+firstLetterOfString=firstName[0]; // this is called indexing and it starts from 0
+console.log(firstLetterOfString);

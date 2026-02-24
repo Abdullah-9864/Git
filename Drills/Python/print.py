f0ec0d97d60fb1880ae0bb11a991db897bb715ca
Abdullah-9864 <abdullahshahid@gmail.com>
2026-02-24 13:25:17 +0500

    files added

--- Drills/Python/print.py
+++ Drills/Python/print.py
@@ -1,3 +1,10 @@
 if(4>3):
     print("4 is greater than 3")
-    
+
+
+print("this is a practice class"); print("this is going to be a fun"); print("this is python class")
+
+print("we will learn oop concepts too", end="     ")
+print("i will create a website demonstrating DSA concepts visually")
+
+print("i am",21,"years old") #this is a comment

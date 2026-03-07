4c997da2bb5e2ff7609a9f4dd53a3a3454a4fbf8
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-03-07 11:35:43 +0500

    files added in WEB

---
+++ WEB/Day1/script.js
@@ -0,0 +1,25 @@
+
+let tasks = [];
+
+function addTask() 
+{
+  const task = document.getElementById("input").value;
+  if (task.trim() === "") 
+  return;
+
+  tasks.push(task);
+  updateTaskList();    // Update the UI
+  document.getElementById("input").value = ""; // Clear input
+}
+
+
+function updateTaskList() {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = ""; // Clear existing tasks
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.textContent = task;
+    taskList.appendChild(li);
+  });
+}
--- WEB/JavaScript/numsAndMaths.js
+++
@@ -1,7 +0,0 @@
-const score= new Number(100);
-console.log(score);
-console.log(score.toString().length);
-console.log(score.toFixed());
-
-console.log(Math.floor(Math.random()*100));
-

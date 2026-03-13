94332b2b7ddd02a18c7e5342d3dd173650c4ae8b
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-03-13 10:22:38 +0500

    files added

--- functions.js
+++ functions.js
@@ -1,7 +1,36 @@
-function sayMyName()
+function sayMyName(num1,num2)
 {
-     console.log("hello world");
+    //  console.log(num1+num2);
+     return num1+num2;
+}
+sayMyName(1,4);
+// console.log(result);
+
+
+
+function loginUserMessage(username)
+{
+    return `${username} just logged in`;
+
 }
 
+console.log(loginUserMessage("Abdullah"));
 
-sayMyName()
+
+
+
+let nrToStr=6;
+nrToStr=String(nrToStr);
+console.log(nrToStr, typeof(nrToStr));
+
+
+
+let strToNr="hello"    ;
+strToNr=Number(strToNr);
+console.log(strToNr, typeof(strToNr));
+
+
+let strToBool = 0;
+strToBool = Boolean(strToBool);
+console.log(strToBool, typeof strToBool);
+

3f343b611b8febca20f4907f3e5e6f2c233ee630
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-03-17 17:54:47 +0500

    files added

---
+++ Drills/JS/chaiOrCode/02_dataTypes.js
@@ -0,0 +1,79 @@
+// "use strict";
+
+// alert(3+3);
+
+num=Number(45);
+console.log(typeof(num));
+
+str=String("hello world");
+console.log(typeof(str));
+
+bool=Boolean(true);
+console.log(typeof(bool));
+
+undef=undefined;
+console.log(typeof(undef));
+
+nulll=null;
+console.log(typeof(null));
+
+
+
+// primitive data types
+// number, bigint, symbol, string, boolean, null, undefined
+
+// non primitive data types (reference)
+// arrays, objects, functions
+
+
+
+
+// browser web events are important
+function  myName()
+{
+    console.log("hello world")
+
+}
+
+
+
+let array=["henry", "abdullah","amir"];
+let myObj=
+{
+    myName:"abdullah",
+    age:21,
+}
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack memory & heap memory
+
+let Name="Abdullah";
+let otherName=Name;
+otherName="Abdullah Shahid";
+console.log(Name);
+console.log(otherName);
+
+
+let obj=
+{
+    Name:"ABD",
+    Age:21,
+}
+
+let obj2=obj;
+
+obj2.Name="abd";
+
+console.log(obj);
+
+
+
+
+
+
+
+
+
+
---
+++ Drills/JS/chaiOrCode/05_strings.js
@@ -0,0 +1,3 @@
+const name="Abdullah Shahid";
+const age=12;
+

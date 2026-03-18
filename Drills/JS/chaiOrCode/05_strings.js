29420df9c98488c9845577827dcffdedc56d1fc9
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-03-18 19:21:17 +0500

    files added

--- Drills/JS/chaiOrCode/05_strings.js
+++ Drills/JS/chaiOrCode/05_strings.js
@@ -1,3 +1,24 @@
 const name="Abdullah Shahid";
 const age=12;
 
+console.log(`My name is ${name} and my age is ${age}`)
+let Name=new String("Abdullah")
+console.log(Name[1])
+console.log(Name.__proto__.toUpperCase());
+// console.log(Name.toUpperCase());
+// console.log(Name.length);
+console.dir(Name);
+// console.log(Name.charAt(2));
+// console.log(Name.indexOf('u'));
+// console.log(Name.slice(-7,3));
+// console.log(Name.substring(-7,3));
+// newName="   abdullah Shahid   ";
+// console.log(newName.trim());
+// const url="https://www.abdullahshahid  .com";
+// console.log(url);
+// console.log(Name.replace("ul", "UL"))
+// console.log(Name.includes("ul"));
+
+
+
+
---
+++ Drills/JS/chaiOrCode/06_nums_maths.js
@@ -0,0 +1,24 @@
+const score=500;
+const marks =new Number(200);
+
+console.log(score);
+console.log(marks);
+console.dir(marks);
+
+console.log(typeof score.toString());
+console.log(score.toFixed(3));
+
+number=100000;
+console.log(number.toLocaleString('en-IN'));
+
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(-5.4));
+console.log(Math.ceil(5.4));
+console.log(Math.floor(5.4));
+console.log(Math.min(5.4));
+
+console.log(Math.random()*100);
+
+

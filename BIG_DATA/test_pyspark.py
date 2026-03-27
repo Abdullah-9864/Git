4750ee15ceb472bb5ff65194874791e49d4baa30
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-03-28 03:07:33 +0500

    files added

--- BIG_DATA/test_pyspark.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("Test").master("local[1]").config("spark.driver.memory", "512m").getOrCreate()
-print("Spark version:", spark.version)  # Print Spark version
-spark.stop()  # Stop Spark

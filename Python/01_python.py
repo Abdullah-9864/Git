97ab7aff18176aa5a01ff3ffbfbaddfcd68de3b6
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-04-04 10:48:44 +0500

    files added

--- Python/01_python.py
+++ Python/01_python.py
@@ -5,3 +5,4 @@ import pyjokes
 print("printing jokes...")
 joke= pyjokes.get_joke()
 print(joke)
+
---
+++ Python/strings.py
@@ -0,0 +1,34 @@
+if 5>2:
+    print("5 is greater than 2")
+
+
+
+# x="awesome"
+def function():
+    global x 
+    x="local"
+    print("value is "+x)
+
+
+
+function()
+
+print(x)
+
+
+y=12
+z=1j
+print(type(y))
+print(type(z))
+
+import random
+print(random.randrange(1,10))
+
+for x in "banana":
+    print(x,end="")
+
+str="Abdullah Shahid"
+print(len(str))
+
+txt="the best things in life"
+print("life" in txt)

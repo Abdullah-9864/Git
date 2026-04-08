b4a3b9d0fcc3fc036c1222cb925c8137ac086b3c
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-04-08 17:26:55 +0500

    files added

---
+++ JS/variables.js
@@ -0,0 +1,14 @@
+const accountId= 145353;
+let accountEmail="abd@hotmail.com";
+// declare + initialize
+var accountPassword="12345";  
+// re-assign
+accountPassword="123456"; 
+// re-declare
+var accountPassword="12345678";
+
+console.log(accountPassword);
+
+
+accountCity="Islamabad";
+

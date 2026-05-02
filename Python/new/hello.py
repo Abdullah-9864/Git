aac703f2505f2ceebd1b77b56a73cb197d7d467c
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-05-02 14:44:21 +0500

    files added

---
+++ Python/new/hello.py
@@ -0,0 +1,16 @@
+print("hello world")
+name="Abdullah"
+print("My name is", name)
+print(name, "is working on great projects")
+
+
+name = input("Enter your name:")
+print(name)
+print(len(name))
+print("A"in name)
+print("hello" not in name)
+
+
+
+
+

d24a292a8b6de46f60ed001b9cb18ec9c4a51618
Abdullah-9864 <abdullahshahid9864@gmail.com>
2026-05-19 10:32:56 +0500

    files added

--- queue.cpp
+++ queue.cpp
@@ -2,12 +2,25 @@
 using namespace std;
 main()
 {
-    int n=345;
+    int n=65;
     int *ptr;
     ptr=&n;
-    cout<<ptr<<endl;
-    cout<<*ptr<<endl;
-    *ptr=234;
-    cout<<*ptr<<endl;
-    
+    cout<<sizeof(int)<<endl;
+    cout<<"address of n: "<<ptr<<"   |    "<<"value of n: "<<*ptr<<endl;
+    char *p1;
+    p1=(char*)ptr;
+    cout<<"address of n: "<<(void*)p1<<"   |    "<<"value of n: "<<*p1<<endl;
+
+    // cout<<*p1;
+
+    // cout<<*ptr<<endl;
+    // *ptr=65;
+    // cout<<*ptr<<endl;
+    // cout<<*(ptr+1)<<endl;
+    // cout<<"------------------------------"<<endl;
+    // char *ptrOne;
+    // cout<<ptrOne<<endl;
+    // ptrOne=(char*)ptr;
+    // cout<<ptrOne<<endl;
+    // cout<<*ptrOne<<endl;
 }
